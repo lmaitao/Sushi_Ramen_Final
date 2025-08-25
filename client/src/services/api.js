fd1eee7fc451cb3da15da3ff093c8adf933d0ef1
Luis Maita <lmaitao25@gmail.com>
2025-08-25 02:52:01 -0400

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'https://sushi-ramen-final.onrender.com/api',
-  timeout: 15000,
+  timeout: 30000,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
