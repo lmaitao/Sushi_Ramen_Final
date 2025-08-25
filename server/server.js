1e6856cfb23fb5c1356ddd32d6a31acff5bc6612
Luis Maita <lmaitao25@gmail.com>
2025-08-24 23:47:37 -0400

    backend
    actualizacion url cors

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middlewares 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://sushi-ramen-final-armc.vercel.app',
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
